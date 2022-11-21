131c1ebf3adf784f1072565e1586416013329143
LuciaRyan <lcryan06@gmail.com>
2022-11-21 23:21:50 +0100

    created counter increase - decrease and reset

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1>Fruitmand bezorgservice</h1>
-    </>
-  );
+
+    const [fruitCounter, setFruitCounter] = React.useState(0)
+
+    const increase = () => {
+        setFruitCounter( fruitCounter + 1)
+    }
+
+    const decrease = () => {
+        setFruitCounter( fruitCounter - 1)
+    }
+
+    const reset = () => {
+        setFruitCounter(0)
+    }
+
+    return (
+
+        <>
+            <div className="fruit-counter">
+                <h1>Fruitmand bezorgservice</h1>
+                <span className="counter-output"/>
+                <div className="button-container">
+                    <button className="control-button" onClick={increase}>+</button>
+                    <button className="control-button" onClick={decrease}>-</button>
+<button className="reset-button" onClick={reset}>reset</button>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;

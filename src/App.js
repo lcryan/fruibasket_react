1c3aab3906197fb9a863818b1853c6ada8d511b5
LuciaRyan <lcryan06@gmail.com>
2022-11-22 00:08:46 +0100

    app.js bananas counter is set up styling is not finalized - first working out all functionalities

--- src/App.js
+++ src/App.js
@@ -3,32 +3,38 @@ import './App.css';
 
 function App() {
 
-    const [fruitCounter, setFruitCounter] = React.useState(0)
+    const [counter, setCounter] = React.useState(0)
 
     const increase = () => {
-        setFruitCounter( fruitCounter + 1)
+        setCounter(counter + 1)
     }
 
     const decrease = () => {
-        setFruitCounter( fruitCounter - 1)
+        if (counter === 0) {
+            alert("You cannot go below zero");
+            return;
+        }
+        setCounter(counter - 1)
     }
 
     const reset = () => {
-        setFruitCounter(0)
+        setCounter(0)
     }
 
     return (
 
         <>
-            <div className="fruit-counter">
-                <h1>Fruitmand bezorgservice</h1>
-                <span className="counter-output"/>
-                <div className="button-container">
-                    <button className="control-button" onClick={increase}>+</button>
-                    <button className="control-button" onClick={decrease}>-</button>
-<button className="reset-button" onClick={reset}>reset</button>
-                </div>
-            </div>
+
+            <h1>Fruitmand bezorgservice</h1>
+
+            <span className="counter-output"/>
+<span className="bananas">
+            <h3> 🍌 Bananen </h3>
+            <button className="control-button" onClick={decrease}>-</button>
+            <div> {counter} </div>
+            <button className="control-button" onClick={increase}>+</button>
+            <button className="reset-button" onClick={reset}>reset</button>
+        </span>
         </>
     );
 }
